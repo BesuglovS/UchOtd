db4c7c2912bc4130a9e4119e7a6d4db079234a12
Безуглов Сергей <Besuglov.S@Gmail.com>
2014-03-03 17:04:45 +0400

    open remote DB

--- UchOtd/Forms/OpenDB.Designer.cs
+++ UchOtd/Forms/OpenDB.Designer.cs
@@ -33,7 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.remoteDatabaseName = new System.Windows.Forms.TextBox();
             this.remoteHost = new System.Windows.Forms.TextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.remoteDB = new System.Windows.Forms.RadioButton();
             this.DatabaseFilename = new System.Windows.Forms.TextBox();
             this.fileDatabase = new System.Windows.Forms.RadioButton();
             this.SQLExpressDatabaseName = new System.Windows.Forms.TextBox();
@@ -41,17 +41,21 @@
             this.actionPanel = new System.Windows.Forms.Panel();
             this.Cancel = new System.Windows.Forms.Button();
             this.openDatabase = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PortNumber = new System.Windows.Forms.TextBox();
             this.ChoosePanel.SuspendLayout();
             this.actionPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // ChoosePanel
             // 
+            this.ChoosePanel.Controls.Add(this.PortNumber);
+            this.ChoosePanel.Controls.Add(this.label3);
             this.ChoosePanel.Controls.Add(this.label2);
             this.ChoosePanel.Controls.Add(this.label1);
             this.ChoosePanel.Controls.Add(this.remoteDatabaseName);
             this.ChoosePanel.Controls.Add(this.remoteHost);
-            this.ChoosePanel.Controls.Add(this.radioButton1);
+            this.ChoosePanel.Controls.Add(this.remoteDB);
             this.ChoosePanel.Controls.Add(this.DatabaseFilename);
             this.ChoosePanel.Controls.Add(this.fileDatabase);
             this.ChoosePanel.Controls.Add(this.SQLExpressDatabaseName);
@@ -86,24 +90,26 @@
             this.remoteDatabaseName.Name = "remoteDatabaseName";
             this.remoteDatabaseName.Size = new System.Drawing.Size(280, 20);
             this.remoteDatabaseName.TabIndex = 6;
+            this.remoteDatabaseName.Enter += new System.EventHandler(this.remoteDatabaseName_Enter);
             // 
             // remoteHost
             // 
             this.remoteHost.Location = new System.Drawing.Point(359, 65);
             this.remoteHost.Name = "remoteHost";
-            this.remoteHost.Size = new System.Drawing.Size(280, 20);
+            this.remoteHost.Size = new System.Drawing.Size(129, 20);
             this.remoteHost.TabIndex = 5;
+            this.remoteHost.Enter += new System.EventHandler(this.remoteHost_Enter);
             // 
-            // radioButton1
+            // remoteDB
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 66);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(209, 17);
-            this.radioButton1.TabIndex = 4;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Удалённая база данных SQLExpress";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.remoteDB.AutoSize = true;
+            this.remoteDB.Location = new System.Drawing.Point(12, 66);
+            this.remoteDB.Name = "remoteDB";
+            this.remoteDB.Size = new System.Drawing.Size(209, 17);
+            this.remoteDB.TabIndex = 4;
+            this.remoteDB.TabStop = true;
+            this.remoteDB.Text = "Удалённая база данных SQLExpress";
+            this.remoteDB.UseVisualStyleBackColor = true;
             // 
             // DatabaseFilename
             // 
@@ -111,6 +117,7 @@
             this.DatabaseFilename.Name = "DatabaseFilename";
             this.DatabaseFilename.Size = new System.Drawing.Size(412, 20);
             this.DatabaseFilename.TabIndex = 3;
+            this.DatabaseFilename.Enter += new System.EventHandler(this.DatabaseFilename_Enter);
             // 
             // fileDatabase
             // 
@@ -129,6 +136,7 @@
             this.SQLExpressDatabaseName.Name = "SQLExpressDatabaseName";
             this.SQLExpressDatabaseName.Size = new System.Drawing.Size(412, 20);
             this.SQLExpressDatabaseName.TabIndex = 1;
+            this.SQLExpressDatabaseName.Enter += new System.EventHandler(this.SQLExpressDatabaseName_Enter);
             this.SQLExpressDatabaseName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SQLExpressDatabaseName_KeyPress);
             // 
             // sqlExpressDB
@@ -172,6 +180,24 @@
             this.openDatabase.UseVisualStyleBackColor = true;
             this.openDatabase.Click += new System.EventHandler(this.openDatabase_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(494, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Порт";
+            // 
+            // PortNumber
+            // 
+            this.PortNumber.Location = new System.Drawing.Point(532, 63);
+            this.PortNumber.Name = "PortNumber";
+            this.PortNumber.Size = new System.Drawing.Size(100, 20);
+            this.PortNumber.TabIndex = 10;
+            this.PortNumber.Text = "1433";
+            this.PortNumber.Enter += new System.EventHandler(this.PortNumber_Enter);
+            // 
             // OpenDB
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -196,7 +222,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox remoteDatabaseName;
         private System.Windows.Forms.TextBox remoteHost;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton remoteDB;
         private System.Windows.Forms.TextBox DatabaseFilename;
         private System.Windows.Forms.RadioButton fileDatabase;
         private System.Windows.Forms.TextBox SQLExpressDatabaseName;
@@ -204,5 +230,7 @@
         private System.Windows.Forms.Panel actionPanel;
         private System.Windows.Forms.Button Cancel;
         private System.Windows.Forms.Button openDatabase;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PortNumber;
     }
 }
--- UchOtd/Forms/OpenDB.cs
+++ UchOtd/Forms/OpenDB.cs
@@ -29,7 +29,21 @@ namespace UchOtd.Forms
         {
             if (sqlExpressDB.Checked)
             {
-                var connectionString = "data source=tcp:127.0.0.1,1433; Database=" + SQLExpressDatabaseName.Text + ";User ID = sa;Password = ghjuhfvvf";
+                var connectionString = "data source=tcp:127.0.0.1,1433; Database=" + SQLExpressDatabaseName.Text + 
+                    ";User ID = " + UchOtd.Properties.Settings.Default.DBUserName +
+                    ";Password = " + UchOtd.Properties.Settings.Default.DBPassword;
+
+                startupForm._repo.ConnectionString = connectionString;
+
+                Close();
+            }
+
+            if (remoteDB.Checked)
+            {
+                var connectionString = "data source=tcp:" + remoteHost.Text +  "," + PortNumber.Text + 
+                    ";Database=" + remoteDatabaseName.Text +
+                    ";User ID = " + UchOtd.Properties.Settings.Default.DBUserName +
+                    ";Password = " + UchOtd.Properties.Settings.Default.DBPassword;
 
                 startupForm._repo.ConnectionString = connectionString;
 
@@ -48,6 +62,31 @@ namespace UchOtd.Forms
         private void OpenDB_Load(object sender, EventArgs e)
         {
             SQLExpressDatabaseName.Focus();
+        }
+
+        private void SQLExpressDatabaseName_Enter(object sender, EventArgs e)
+        {
+            sqlExpressDB.Checked = true;
+        }
+
+        private void DatabaseFilename_Enter(object sender, EventArgs e)
+        {
+            fileDatabase.Checked = true;
+        }
+
+        private void remoteHost_Enter(object sender, EventArgs e)
+        {
+            remoteDB.Checked = true;
+        }
+
+        private void PortNumber_Enter(object sender, EventArgs e)
+        {
+            remoteDB.Checked = true;
+        }
+
+        private void remoteDatabaseName_Enter(object sender, EventArgs e)
+        {
+            remoteDB.Checked = true;
         }
     }
 }
