51ab778bb0fd00cc8a6922f5b39218eaec159aa3
Безуглов Сергей <Besuglov.S@Gmail.com>
2014-03-11 10:21:20 +0400

    Correcting ServerList

--- UchOtd/StartupForm.cs
+++ UchOtd/StartupForm.cs
@@ -113,8 +113,8 @@ namespace UchOtd
         private void InitRepositories()
         {
             var ServerList = new List<string>() { 
-                "127.0.0.1", 
-                "10.13.3.1"
+                "uch-otd-disp",
+                "127.0.0.1"
             };
 
             bool successPing = false;
